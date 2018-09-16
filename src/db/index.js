5fd3ab8e4e4d567c0f6bac0fec8cd6126a78f8b0
hptg1994 <hptg1994@gmail.com>
2018-09-15 23:35:32 -0400

    adding a new project

---
+++ src/db/index.js
@@ -0,0 +1,6 @@
+import PouchDB from 'pouchdb-browser';
+ const db = {
+  series: new PouchDB('series'),
+  episodes: new PouchDB('episodes'),
+};
+ export default db;
---
+++ src/pages/home.js
@@ -0,0 +1,46 @@
+// npm packages
+import React from 'react';
+import {Observable} from 'rxjs';
+// our packages
+import db from '../db';
+import {Crunchyroll} from '../api/crunchroll';
+// our components
+import Series from '../components/series/series'
+
+export default class Home extends React.Component {
+  constructor() {
+    super();
+     this.state = {
+      series: [],
+    };
+     // trigger list update
+    Crunchyroll.getAllSeries();
+  }
+   componentDidMount() {
+    this.sub = Observable.fromEvent(
+      db.series.changes({
+        since: 0,
+        live: true,
+        include_docs: true,
+      }),
+      'change'
+    )
+      .filter(change => !change.deleted)
+      .map(change => change.doc)
+      .scan((acc, doc) => acc.concat([doc]), [])
+      .debounceTime(1000)
+      .subscribe(series => console.log(series)&&this.setState({series}));
+  }
+   componentWillUnmount() {
+    this.sub.unsubscribe();
+  }
+   render() {
+    
+    const {series} = this.state;
+     return (
+      <div>
+        {series.map(s => <Series key={s._id} series={s} />)}
+      </div>
+    );
+  }
+}
